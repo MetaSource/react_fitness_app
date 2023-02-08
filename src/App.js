734e13d5918c0e1be74dab9aa20f83eb1deeaecb
MetaSource <jsal91@gmail.com>
2023-02-08 10:00:53 -0800

    Style navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './App.css';
+import {Route, Routes} from 'react-router-dom'
 import ExerciseDetail from './pages/ExerciseDetail';
 import Home from './pages/Home';
+import { Box } from '@mui/material'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import { fromBits } from 'long';
 
 
 const App = () => {
   return (
-    <Box width="400px" sx={{widht: {xl: '1488px'}}} m="auto">
-        Navbar 
+    <Box width="400px" sx={{width: {xl: '1488px'}}} m="auto">
+        <Navbar/>
         <Routes>
             <Route path="/" element={<Home/>}/> 
             <Route path="/exercise/:id" element={<ExerciseDetail/>}/>
         </Routes>
+        <Footer />
     </Box>
   )
 }
